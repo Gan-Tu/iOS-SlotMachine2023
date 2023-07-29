c7a8c60d0cd91bdf3f84a4cc2abdb100c5a483e6
Gan Tu <github@tugan.dev>
2023-07-29 17:39:52 -0400

    Initial Commit

---
+++ Slot Machine Game 2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slot Machine Game 2023
+//
+//  Created by Gan Tu on 7/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slot Machine Game 2023/Slot_Machine_Game_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine_Game_2023App.swift
+//  Slot Machine Game 2023
+//
+//  Created by Gan Tu on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine_Game_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
